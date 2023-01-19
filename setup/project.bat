8efb38d596506217df3c949f435c49a89a683b04
Alex Culango <wizardoncouch@gmail.com>
2023-01-19 15:38:17 +0800

    comment the web service

--- setup/project.bat
+++ setup/project.bat
@@ -4,8 +4,8 @@ python -m venv venv
 call venv\Scripts\activate
 pip install -r "requirements.txt"
 
-set FLASK_APP=app.py && flask run --host=0.0.0.0
+rem set FLASK_APP=app.py && flask run --host=0.0.0.0
 set FLASK_APP=app.py && flask scheduled
 
 schtasks /create /sc minute /mo 5 /tn "POS Sync" /tr "set FLASK_APP=app.py && %~dp0\..\venv\Scripts\flask scheduled"
-schtasks /create /sc onstart /tn "POS App" /tr "set FLASK_APP=app.py && %~dp0\..\venv\Scripts\flask run --host=0.0.0.0"
+rem schtasks /create /sc onstart /tn "POS App" /tr "set FLASK_APP=app.py && %~dp0\..\venv\Scripts\flask run --host=0.0.0.0"
