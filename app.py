93a3922351a245cd92f2a743d98b0259ac05b124
Alex Culango <wizardoncouch@gmail.com>
2023-01-19 16:33:00 +0800

    update prepared statements

--- app.py
+++ app.py
@@ -31,20 +31,20 @@ def scheduled():
         for product in products.json():
 
             checkCategory = db.cursor(prepared=True)
-            checkCategory.execute("SELECT count(*) FROM tblmenulist WHERE `class`='%s' and iscategory=1", (product['category'],))
+            checkCategory.execute("SELECT count(*) FROM tblmenulist WHERE `class`=%s and iscategory=1", (product['category'],))
             category = checkCategory.fetchone()
             if category[0] == 0:
                 insertCategory = db.cursor(prepared=True)
                 insertCategory.execute("""INSERT INTO tblmenulist   (`class`,   `iscategory`,   `skincolor`,    `fontcolor`,    `dlock`) 
-                                                        VALUES      ('%s',      '%s',           '%s',           '%s',           NOW())""",
+                                                        VALUES      (%s,      %s,           %s,           %s,           NOW())""",
                                                                     (product['category'], 1, '-8355712','-16777216'))
 
             checkGroup = db.cursor(prepared=True)
-            checkGroup.execute("SELECT count(*) FROM `tblmenugrp` WHERE `grp`='%s'", (product['group'],))
+            checkGroup.execute("SELECT count(*) FROM `tblmenugrp` WHERE `grp`=%s", (product['group'],))
             group = checkGroup.fetchone()
             if group[0] == 0:
                 inserGroup = db.cursor(prepared=True)
-                inserGroup.execute("INSERT INTO `tblmenugrp`(`grp`, `dlock`) VALUES('%s',NOW())",(product['group'],))
+                inserGroup.execute("INSERT INTO `tblmenugrp`(`grp`, `dlock`) VALUES(%s,NOW())",(product['group'],))
 
 
             fetchItem = db.cursor(dictionary=True)
@@ -52,12 +52,12 @@ def scheduled():
             p = fetchItem.fetchone()
             if p and p['amt'] != product['price']:
                     updateItem = db.cursor(prepared=True)
-                    updateItem.execute("UPDATE `item` set `amt`='%s' WHERE `itemid`='%s'",(product['price'], p['itemid']))
+                    updateItem.execute("UPDATE `item` set `amt`=%s WHERE `itemid`=%s",(product['price'], p['itemid']))
                     print('Product updated...')
             if p is None:
                 insert = db.cursor(prepared=True)
                 insert.execute("""INSERT INTO `item`(`barcode`,   `itemname`,   `shortname`,   `groupid`,    `part`,   `class`,      `amt`,        `uom`,    `dlock`) 
-                                            VALUES  ('%s',        '%s',         '%s',          '%s',         'MENU',   '%s',         '%s',         '%s',     NOW())""",
+                                            VALUES  (%s,        %s,         %s,          %s,         'MENU',   %s,         %s,         %s,     NOW())""",
                                                     (product['uid'], product['name'], product['name'], product['group'], product['category'], product['price'], product['unit']))
                 print('New product inserted...')
 
