59946a87284ad92e5cb6c072a455904bb87d3469
Alex Culango <wizardoncouch@gmail.com>
2023-01-20 11:28:34 +0800

    manage batch script

--- script-install.bat
+++ script-install.bat
@@ -1,14 +1,15 @@
 @echo off
 
+cd %~dp0 
+
 rem --Download python installer
 curl "https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe" -o python-installer.exe
 
 rem --Install python
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 
 
-call %~dp0\script-resetvars.bat
+call script-resetvars.bat
 
-cd %~dp0 
 python -m venv venv
 call venv\Scripts\activate
 pip install -r "requirements.txt"
