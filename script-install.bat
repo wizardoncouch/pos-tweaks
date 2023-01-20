880242804a226bcba1c76c40472b83827bbbbddb
Alex Culango <wizardoncouch@gmail.com>
2023-01-20 11:31:30 +0800

    !!

--- script-install.bat
+++ script-install.bat
@@ -14,8 +14,8 @@ python -m venv venv
 call venv\Scripts\activate
 pip install -r "requirements.txt"
 
-schtasks /create /sc minute /mo 5 /tn "POS Sync" /tr "%~dp0\script-cron.bat"
-schtasks /create /sc onstart /tn "POS App" /tr "%~dp0\script-serve.bat"
+schtasks /create /sc minute /mo 5 /tn "POS Sync" /tr "%~dp0script-cron.bat"
+schtasks /create /sc onstart /tn "POS App" /tr "%~dp0script-serve.bat"
 
 echo Setup Complete
 pause
