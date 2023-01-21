f71560e989d5792705c66acf7df345b1df72a69c
Alex Culango <wizardoncouch@gmail.com>
2023-01-21 12:06:14 +0800

    orders and cron updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response
 from mysql import connector
 import requests
 import click
@@ -16,9 +16,94 @@ db = connector.connect(
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    print(db)
-    return "<p>Hello, World!</p>"
+def floors():
+    args = request.args
+    data = {
+        "floor": None,
+        "floors": [],
+        "tables": []
+    }
+
+    getFloors = db.cursor(dictionary=True)
+    getFloors.execute("SELECT DISTINCT(flr) as floor FROM client WHERE isconsignee=1")
+    for row in getFloors.fetchall():
+        data['floors'].append({
+            "name": row['floor']
+        })
+    floor = args.get('floor')
+    if floor is None and data['floors']:
+        floor = data['floors'][0]['name']
+    
+    if floor:
+        data['floor'] = floor
+        getTables = db.cursor(dictionary=True, prepared=True) 
+        getTables.execute("""SELECT `clientname`, `client`, `clientid`, `locx`, `locy`, (SELECT count(*) from salestran WHERE client=client.client) as `ordercount` FROM `client` WHERE flr=%s""", (floor,))
+        for table in getTables.fetchall():
+            data['tables'].append({
+                "id": table['clientid'],
+                "name": table['clientname'],
+                "left": table['locx'],
+                "top": table['locy'],
+                "inuse": True if table['ordercount'] > 0 else False
+            })
+
+    return render_template('tables.html', data = data)
+
+@app.route("/table/<id>")
+def tables(id):
+    args = request.args
+    getCategories = db.cursor(dictionary=True)
+    getCategories.execute("SELECT id, class as name FROM tblmenulist where isinactive=0 and iscategory=1 order by class asc")
+    categories = getCategories.fetchall()
+
+    products = []
+    subProducts = []
+    if args.get('category'):
+        getGroups = db.cursor(dictionary=True, prepared=True)
+        getGroups.execute("SELECT DISTINCT(groupid) as itemname, '' as barcode FROM `item` WHERE `class`=%s AND `groupid` > ''", (args.get('category'),))
+        groups = getGroups.fetchall()
+        if groups:
+            products = groups
+            if args.get('group'):
+                getProductsByGroup = db.cursor(dictionary=True, prepared=True)
+                getProductsByGroup.execute("SELECT * FROM item WHERE `class`=%s AND `groupid`=%s ORDER by itemname ASC", (args.get('category'), args.get('group')))
+                subProducts = getProductsByGroup.fetchall()
+
+            #for not grouped products
+            getProducts = db.cursor(dictionary=True, prepared=True)
+            getProducts.execute("SELECT * FROM item WHERE `class`=%s and groupid='' ORDER by itemname ASC", (args.get('category'),))
+            for p in getProducts.fetchall():
+                products.append(p)
+        else:
+            getProducts = db.cursor(dictionary=True, prepared=True)
+            getProducts.execute("SELECT * FROM item WHERE `class`=%s ORDER by itemname ASC", (args.get('category'),))
+            products = getProducts.fetchall()
+
+
+    getTable = db.cursor(dictionary=True, prepared=True)
+    getTable.execute("SELECT * FROM client WHERE clientid=%s", (id,))
+    table = getTable.fetchone()
+    if table:
+        getOrders = db.cursor(dictionary=True, prepared=True)
+        getOrders.execute("SELECT * FROM salestran where client=%s", (table['client'],))
+        orders = getOrders.fetchall()
+        total = 0
+        printable = False
+        for order in orders:
+            total += order['isamt']
+            if order['isprint'] == 0:
+                printable = True
+        return render_template('order.html', data={
+            "categories": categories,
+            "products": products,
+            "subProducts": subProducts,
+            "table":table, 
+            "orders": orders, 
+            "total": total, 
+            "printable": printable
+        })
+    else:
+        return make_response("Table not found", 404)
 
 @app.cli.command()
 @click.option('--b')
@@ -48,10 +133,15 @@ def scheduled(b):
             fetchItem.execute("SELECT * FROM `item` WHERE `barcode`='{uid}'".format(uid=product['uid']))
             p = fetchItem.fetchone()
             if p is None:
+                getClassPrinter = db.cursor(prepared=True, dictionary=True)
+                getClassPrinter.execute("SELECT model FROM item WHERE `class`=%s and `model` > '' LIMIT 1", (product['category'],))
+                classPrinter = getClassPrinter.fetchone()
+                model = classPrinter['model'] if classPrinter else ''
+                
                 insert = db.cursor(prepared=True)
-                insert.execute("""INSERT INTO `item`(`barcode`, `itemname`, `shortname`, `groupid`, `part`, `class`, `uom`, `dlock`, `isinactive`) 
-                                            VALUES  (%s,        %s,         %s,          %s,        'MENU', %s,      %s,    NOW(),   1           )""",
-                                                    (product['uid'], product['name'], product['name'], product['group'], product['category'], product['unit']))
+                insert.execute("""INSERT INTO `item`(`barcode`, `itemname`, `shortname`, `groupid`, `part`, `class`, `uom`, `dlock`, `isinactive`, `taxable`, `model`) 
+                                            VALUES  (%s,        %s,         %s,          %s,        'MENU', %s,      %s,    NOW(),   1,            1,         %s     )""",
+                                                    (product['uid'], product['name'], product['name'], product['group'], product['category'], product['unit'], model))
                 print("{name} is inserted...".format(name=product['name']))
                 fetchItem = db.cursor(dictionary=True)
                 fetchItem.execute("SELECT * FROM `item` WHERE itemid=last_insert_id()")
