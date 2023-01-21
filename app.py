561f65bfd07e84bec30d6ddc2a4622994d647baf
Alex Culango <wizardoncouch@gmail.com>
2023-01-21 15:34:12 +0800

    update for ordering app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect
 from mysql import connector
 import requests
 import click
@@ -7,9 +7,9 @@ from datetime import datetime
 db = connector.connect(
     host="localhost",
     user="root",
-    password="x1root99",
+    password="mjm",
     database="lite",
-    port=3306
+    port=3309
 )
 
 
@@ -60,7 +60,7 @@ def tables(id):
     subProducts = []
     if args.get('category'):
         getGroups = db.cursor(dictionary=True, prepared=True)
-        getGroups.execute("SELECT DISTINCT(groupid) as itemname, '' as barcode FROM `item` WHERE `class`=%s AND `groupid` > ''", (args.get('category'),))
+        getGroups.execute("SELECT DISTINCT(groupid) as itemname, class, '' as barcode FROM `item` WHERE `class`=%s AND `groupid` > ''", (args.get('category'),))
         groups = getGroups.fetchall()
         if groups:
             products = groups
@@ -90,7 +90,7 @@ def tables(id):
         total = 0
         printable = False
         for order in orders:
-            total += order['isamt']
+            total += float(order['isamt']) * float(order['isqty'])
             if order['isprint'] == 0:
                 printable = True
         return render_template('order.html', data={
@@ -105,10 +105,84 @@ def tables(id):
     else:
         return make_response("Table not found", 404)
 
+@app.route("/transaction", methods=['POST'])
+def transaction():
+    if request.form.get('id'):
+        getTransaction = db.cursor(prepared=True, dictionary=True)
+        getTransaction.execute("SELECT * FROM salestran WHERE line=%s", (request.form.get('id'),))
+        transaction = getTransaction.fetchone()
+        if transaction and request.form.get('qty'):
+            updateTransaction = db.cursor(prepared=True)
+            updateTransaction.execute("UPDATE salestran set isqty=%s WHERE line=%s", (request.form.get('qty'), transaction['line']))
+
+    if request.form.get('barcode'):
+        getItem = db.cursor(prepared=True, dictionary=True)
+        getItem.execute("SELECT * FROM item WHERE barcode=%s", (request.form.get('barcode'),))
+        item = getItem.fetchone()
+        getTable = db.cursor(prepared=True, dictionary=True)
+        getTable.execute("SELECT * FROM `client` WHERE `client`=%s", (request.form.get('table'),))
+        table = getTable.fetchone()
+        if table is None:
+            return make_response("Table not specified", 404)
+        
+        getTransaction = db.cursor(prepared=True, dictionary=True)
+        getTransaction.execute("SELECT * FROM salestran WHERE `client`=%s LIMIT 1", (table['client'],))
+        transaction = getTransaction.fetchone()
+        if transaction:
+            osno = transaction['osno']
+            ccode = transaction['ccode']
+            screg = transaction['screg']
+            scsenior = transaction['scsenior']
+            grp = transaction['grp']
+            waiter = transaction['waiter']
+            source = transaction['source']
+        else:
+            getOS = db.cursor(prepared=True)
+            getOS.execute("INSERT INTO osnumber(tableno) VALUES(%s)", (table['client'],))
+            osno = getOS.lastrowid
+            ccode = 'WALK-IN'
+            screg = 10
+            scsenior = 10
+            grp = 'A'
+            waiter = 'Administrator'
+            source = 'WH00001'
+        checkTransaction = db.cursor(prepared=True, dictionary=True)
+        checkTransaction.execute("SELECT * FROM salestran WHERE client=%s and barcode=%s", (table['client'], item['barcode']))
+        existingTransation = checkTransaction.fetchone()
+        if existingTransation:
+            updateTransaction = db.cursor(prepared=True)
+            updateTransaction.execute("UPDATE salestran SET isqty=isqty+1 WHERE line=%s", (existingTransation['line'],))
+        else:
+            insertTransaction = db.cursor(prepared=True)
+            insertTransaction.execute("""INSERT INTO salestran(`client`, `clientname`, `barcode`, `itemname`, `isamt`, `isqty`, `uom`, `grp`, `waiter`, `osno`, `screg`, `scsenior`, `ccode`, `source`, `isprint`, dateid)
+                                                        VALUES(%s,       %s,           %s,        %s,         %s,      1,       %s,    %s,    %s,       %s,     %s,      %s,         %s,      %s,       0,         CURRENT_DATE()       )""",
+                                                            (table['client'], table['clientname'], item['barcode'], item['itemname'], item['amt'], item['uom'], grp, waiter, osno, screg, scsenior, ccode, source))
+    return redirect(request.referrer)
+
+@app.route("/transaction/delete", methods=['POST'])
+def transaction_delete():
+    if request.form.get('id'):
+        getTransaction = db.cursor(prepared=True, dictionary=True)
+        getTransaction.execute("SELECT * FROM salestran WHERE line=%s", (request.form.get('id'),))
+        transaction = getTransaction.fetchone()
+        deleteTransaction = db.cursor(prepared=True)
+        deleteTransaction.execute("DELETE FROM salestran WHERE line=%s", (transaction['line'],))
+
+        getTableTransactions = db.cursor(prepared=True, dictionary=True)
+        getTableTransactions.execute("SELECT count(*) as cnt FROM salestran WHERE client=%s", (transaction['client'],))
+        transactions = getTableTransactions.fetchone()
+        if transactions['cnt'] == 0:
+            deleteOSNumber = db.cursor(prepared=True)
+            deleteOSNumber.execute("DELETE FROM osnumber WHERE tableno=%s", (transaction['client'],))
+
+    return redirect(request.referrer)
+
 @app.cli.command()
 @click.option('--b')
 def scheduled(b):
     products = requests.get('https://pp.d3.net/api.php?action=products&branch=' + b)
+    productInactive = db.cursor()
+    productInactive.execute("UPDATE `item` set `isinactive`=1 AND `barcode` > 0")
     if products:
         for product in products.json():
 
@@ -139,18 +213,21 @@ def scheduled(b):
                 model = classPrinter['model'] if classPrinter else ''
                 
                 insert = db.cursor(prepared=True)
-                insert.execute("""INSERT INTO `item`(`barcode`, `itemname`, `shortname`, `groupid`, `part`, `class`, `uom`, `dlock`, `isinactive`, `taxable`, `model`) 
-                                            VALUES  (%s,        %s,         %s,          %s,        'MENU', %s,      %s,    NOW(),   1,            1,         %s     )""",
-                                                    (product['uid'], product['name'], product['name'], product['group'], product['category'], product['unit'], model))
+                insert.execute("""INSERT INTO `item`(`barcode`, `itemname`, `shortname`, `groupid`, `part`, `class`, `uom`, `dlock`, `amt`,  `taxable`, `model`) 
+                                            VALUES  (%s,        %s,         %s,          %s,        'MENU', %s,      %s,    NOW(),   %s,     1,         %s     )""",
+                                                    (product['uid'], product['name'], product['name'], product['group'], product['category'], product['unit'], product['price'], model))
                 print("{name} is inserted...".format(name=product['name']))
                 fetchItem = db.cursor(dictionary=True)
                 fetchItem.execute("SELECT * FROM `item` WHERE itemid=last_insert_id()")
                 p = fetchItem.fetchone()
             
-            if float(product['price']) > 0 and float(product['price'] != float(p['amt'])):
+            updateItem = db.cursor(prepared=True)
+            updateItem.execute("UPDATE `item` set `isinactive`=0 WHERE `itemid`=%s",(p['itemid'],))
+
+            if float(product['price'] != float(p['amt'])):
                 updateItem = db.cursor(prepared=True)
-                updateItem.execute("UPDATE `item` set `amt`=%s, `isinactive`=0 WHERE `itemid`=%s",(product['price'], p['itemid']))
-                print("{name} is updated...".format(name=product['name']))
+                updateItem.execute("UPDATE `item` set `amt`=%s WHERE `itemid`=%s",(product['price'], p['itemid']))
+                print("{name} price is updated...".format(name=product['name']))
 
             #update if the category is changed
             if p['class'] != product['category']:
