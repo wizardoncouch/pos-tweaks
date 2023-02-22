84b8d606794cd04280d0380289e5eb0bb621bdf2
Alex Culango <wizardoncouch@gmail.com>
2023-02-22 11:27:12 +0800

    fix sync script

--- sync.py
+++ sync.py
@@ -1,9 +1,8 @@
 import os,sys
 import requests
-from datetime import datetime
 import json
 from dotenv import load_dotenv
-from app import db
+from app import db, app
 from sqlalchemy import text
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
@@ -56,120 +55,101 @@ elif action == "items":
     # productInactive = db.cursor()
     # productInactive.execute("UPDATE `item` set `isinactive`=1 WHERE `barcode` != ''")
     
-    itemids = []
-    if len(products.json()):
-        for product in products.json():
+    with app.app_context():
+        itemids = []
+        if len(products.json()):
+            for product in products.json():
 
-            category = db.session.execute(text("SELECT count(*) as cnt FROM tblmenulist WHERE `class`='{cl}' and iscategory=1".format(cl=product['category']))).fetchone()
-            if category.cnt == 0:
-                db.session.execute(text("""INSERT INTO tblmenulist   (`class`,   `iscategory`,      `skincolor`,    `fontcolor`,    `dlock`) 
-                                                        VALUES      ('{cl}',      '{iscategory}',   '{skin}',       '{font}',        NOW())"""
-                                    .format(cl=product['category'], iscategory=1, skin='-8355712', font='-16777216')))
+                category = db.session.execute(text("SELECT count(*) as cnt FROM tblmenulist WHERE `class`='{cl}' and iscategory=1".format(cl=product['category']))).fetchone()
+                if category.cnt == 0:
+                    db.session.execute(text("""INSERT INTO tblmenulist   (`class`,   `iscategory`,      `skincolor`,    `fontcolor`,    `dlock`) 
+                                                            VALUES      ('{cl}',      '{iscategory}',   '{skin}',       '{font}',        NOW())"""
+                                        .format(cl=product['category'], iscategory=1, skin='-8355712', font='-16777216')))
 
-            group = db.session.execute(text("SELECT count(*) as cnt FROM `tblmenugrp` WHERE `grp`='{group}'".format(group=product['group']))).fetchone()
-            if group.cnt == 0:
-                db.session.execute(text("INSERT INTO `tblmenugrp`(`grp`, `dlock`) VALUES('{group}',NOW())".format(product['group'])))
+                group = db.session.execute(text("SELECT count(*) as cnt FROM `tblmenugrp` WHERE `grp`='{group}'".format(group=product['group']))).fetchone()
+                if group.cnt == 0:
+                    db.session.execute(text("INSERT INTO `tblmenugrp`(`grp`, `dlock`) VALUES('{group}',NOW())".format(product['group'])))
 
 
-            p = None
-            with db.cursor(dictionary=True) as cursor:
-                cursor.execute("SELECT * FROM `item` WHERE `barcode`='{uid}'".format(uid=product['uid']))
-                p = cursor.fetchone()
-                cursor.close()
+                p = db.session.execute(text("SELECT *, `class` as category FROM `item` WHERE `barcode`='{uid}'".format(uid=product['uid']))).fetchone()
                 if p is None:
-                    with db.cursor(prepared=True, dictionary=True) as cursor:
-                        cursor.execute("SELECT model FROM item WHERE `class`=%s and `model` > '' LIMIT 1", (product['category'],))
-                        classPrinter = cursor.fetchone()
-                        model = classPrinter['model'] if classPrinter else ''
+                    classPrinter = db.session.execute(text("SELECT model FROM item WHERE `class`='{cl}' and `model` > '' LIMIT 1".format(cl=product['category']))).fetchone()
+                    model = classPrinter.model if classPrinter else ''
                     
-                    with db.cursor(prepared=True, dictionary=True) as cursor:
-                        cursor.execute("""INSERT INTO `item`(`barcode`, `itemname`, `shortname`, `groupid`, `part`, `class`, `uom`, `dlock`, `amt`,  `taxable`, `model`) 
-                                                    VALUES  (%s,        %s,         %s,          %s,        'MENU', %s,      %s,    NOW(),   %s,     1,         %s     )""",
-                                                            (product['uid'], product['name'], product['name'], product['group'], product['category'], product['unit'], product['price'], model))
-                        print("{name} is inserted...".format(name=product['name']))
-                        cursor.execute("SELECT * FROM `item` WHERE itemid=last_insert_id()")
-                        p = cursor.fetchone()
-                        cursor.execute("SELECT * FROM `itemdlock` WHERE `itemid`=%s", (p['itemid'], ))
-                        dlock = cursor.fetchone()
-                        if dlock:
-                            cursor.execute("UPDATE `itemdlock` SET `dlock`=%s WHERE `itemid`=%s", (p['dlock'], p['itemid']))
-                        else:
-                            cursor.execute("INSERT INTO `itemdlock`(`itemid`, `dlock`) VALUES(%s, %s)", (p['itemid'], p['dlock']))
-                        cursor.close()
-            
-            if float(product['price']) != float(p['amt']):
-                with db.cursor(prepared=True) as cursor:
-                    cursor.execute("UPDATE `item` set `amt`=%s WHERE `itemid`=%s",(product['price'], p['itemid']))
+                    inserted = db.session.execute(text("""INSERT INTO `item`(`barcode`,    `itemname`,     `shortname`,    `groupid`,      `part`, `class`,    `uom`,      `dlock`,    `amt`,      `taxable`, `model`  ) 
+                                                        VALUES  ('{barcode}',   '{itemname}',   '{shortname}',  '{groupid}',    'MENU', '{cl}',     '{unit}',    NOW(),     '{amt}',    1,         '{model}')"""
+                                                        .format(barcode=product['uid'], itemname=product['name'], shortname=product['name'], groupid=product['group'], cl=product['category'], unit=product['unit'], amt=product['price'], model=model)))
+                    print("{name} is inserted...".format(name=product['name']))
+                    p = db.session.execute(text("SELECT *, `class` as category FROM `item` WHERE itemid='{itemid}'".format(itemid=inserted.lastrowid)))
+                    dlock = db.session.execute(text("SELECT * FROM `itemdlock` WHERE `itemid`='{itemid}'".format(itemid=p.itemid))).fetchone()
+                    if dlock:
+                        db.session.execute(text("UPDATE `itemdlock` SET `dlock`='{dlock}' WHERE `itemid`='{itemid}'".format(dlock=p.dlock, itemid=p.itemid)))
+                    else:
+                        db.session.execute(text("INSERT INTO `itemdlock`(`itemid`, `dlock`) VALUES('{itemid}', '{dlock}')".format(itemid=p.itemid, dlock=p.dlock)))
+                
+                if float(product['price']) != float(p.amt):
+                    db.session.execute(text("UPDATE `item` set `amt`='{amt}' WHERE `itemid`='{itemid}'".format(amt=product['price'], itemid=p.itemid)))
                     print("{name} price is updated...".format(name=product['name']))
-                    cursor.close()
 
-            #update if the category is changed
-            if p['class'] != product['category']:
-                with db.cursor(prepared=True) as cursor:
-                    cursor.execute("UPDATE `item` set `class`=%s WHERE `itemid`=%s",(product['category'], p['itemid']))
+                #update if the category is changed
+                if p.category != product['category']:
+                    db.session.execute(text("UPDATE `item` set `class`='{cl}' WHERE `itemid`='{itemid}'".format(cl=product['category'], itemid=p.itemid)))
                     print("{name} category updated to {category}...".format(name=product['name'], category=product['category']))
-                    cursor.close()
 
-            #update if the group is changed
-            if p['groupid'] != product['group']:
-                with db.cursor(prepared=True) as cursor:
-                    cursor.execute("UPDATE `item` set `groupid`=%s WHERE `itemid`=%s",(product['group'], p['itemid']))
+                #update if the group is changed
+                if p.groupid != product['group']:
+                    db.session.execute(text("UPDATE `item` set `groupid`='{group}' WHERE `itemid`='{itemid}'".format(group=product['group'], itemid=p.itemid)))
                     print("{name} group updated to {group}...".format(name=product['name'], group=product['group']))
-                    cursor.close()
-            
-            itemids.append(p['itemid'])
+                
+                itemids.append(p.itemid)
 
-    else:
-        print('No products found, please check your config file for the branch id')
-    
-    if itemids:
-        print(len(itemids))
-        format_ids = "({})".format(','.join([str(i) for i in itemids]))
-        with db.cursor() as cursor:
-            cursor.execute("UPDATE `item` set `isinactive`=1 WHERE `itemid` NOT IN %s" % format_ids)
-            print("products set to inactive = {}".format(cursor.rowcount))
-            cursor.close()
+        else:
+            print('No products found, please check your config file for the branch id')
+        
+        if itemids:
+            print(len(itemids))
+            format_ids = "({})".format(','.join([str(i) for i in itemids]))
+            update = db.session.execute(text("UPDATE `item` set `isinactive`=1 WHERE `itemid` NOT IN %s" % format_ids))
+            print("products set to inactive = {}".format(update.rowcount))
 
-    #set the category to inactive if there are no active item found
-    with db.cursor() as cursor:
-        cursor.execute("UPDATE tblmenulist set isinactive=1 WHERE (SELECT count(*) FROM item WHERE iscategory=1 and `class`=tblmenulist.class and isinactive=0) = 0")
-        cursor.close()
+        #set the category to inactive if there are no active item found
+        db.session.execute(text("UPDATE tblmenulist set isinactive=1 WHERE (SELECT count(*) FROM item WHERE iscategory=1 and `class`=tblmenulist.class and isinactive=0) = 0"))
 
 elif action == "sales":
-    print("Uploading sales...")
-    response = requests.get('https://pp.d3.net/api.php?action=last-sync&branch=' + branch_id, headers=requests_headers)
-    if response is None:
-        exit('Cannot connect...')
+    with app.app_context():
 
-    last = dict(response.json())
-    if last['code'] != 200:
-        exit('Error code: '+str(last['code']))
-    
-    last_number = last['number']
+        print("Uploading sales...")
+        response = requests.get('https://pp.d3.net/api.php?action=last-sync&branch=' + branch_id, headers=requests_headers)
+        print(response.json())
+        if response is None:
+            exit('Cannot connect...')
 
-    with db.cursor(prepared=True, dictionary=True) as cursor:
-        cursor.execute("SELECT * FROM `glhead` WHERE `billnumber`>'' AND `docno` > %s ORDER BY `docno` ASC", (last_number,))
+        last = dict(response.json())
+        if last['code'] != 200:
+            exit('Error code: '+str(last['code']))
+        
+        last_number = ''#last['number']
+
+        sql = text("SELECT * FROM `glhead` WHERE `billnumber`>'' AND `docno` > '{docno}' ORDER BY `docno` ASC".format(docno=last_number))
         sales = []
-        for sale in cursor.fetchall():
-            cursor.execute("""SELECT g.*,i.barcode 
+        for sale in db.session.execute(sql):
+            tsql = text("""SELECT g.*,i.barcode 
                                 FROM `glstock` as g 
                                 LEFT JOIN `item` as i ON i.itemid = g.itemid
-                                WHERE g.`trno`=%s""" % sale['trno'])
+                                WHERE g.`trno`=%s""" % sale.trno)
             sales.append(dict({
-                'number': sale['docno'],
-                'created': sale['printtime'].strftime("%Y-%m-%d %H:%M:%S") if sale['printtime'] else '',
-                'total': float(sale['amt']),
-                'remarks': sale['rem'],
+                'number': sale.docno,
+                'created': sale.printtime.strftime("%Y-%m-%d %H:%M:%S") if sale.printtime else '',
+                'total': float(sale.amt),
+                'remarks': sale.rem,
                 'items': [dict({
-                    'uid': item['barcode'] if item['barcode'] else 0,
-                    'name': item['itemname'],
-                    'amount': float(item['ext']),
-                    'qty': float(item['isqty']),
-                    'created': item['createdate'].strftime("%Y-%m-%d %H:%M:%S") if item['createdate'] else ''
-                }) for item in cursor.fetchall()]
+                    'uid': item.barcode if item.barcode else 0,
+                    'name': item.itemname,
+                    'amount': float(item.ext),
+                    'qty': float(item.isqty),
+                    'created': item.createdate.strftime("%Y-%m-%d %H:%M:%S") if item.createdate else ''
+                }) for item in db.session.execute(tsql)]
             }))
-
-        cursor.close()
 
         payload = {
             "action": "sales",
@@ -178,7 +158,6 @@ elif action == "sales":
         }
 
         response = requests.post("https://pp.d3.net/api.php", data=payload, headers=requests_headers)
-        
         print(response.json())
 
 elif action == 'DynDNS':
@@ -189,5 +168,3 @@ elif action == "test":
     print('testing ...')
     response = requests.get('https://pp.d3.net/api.php?action=test', headers=requests_headers)
     print(response.json())
-if db is not None:
-    db.close()
